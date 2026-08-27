849efcba6f64aceaf0da80f92c580a5e9b2808a4
Nicolas <nicolasalejocampos@outlook.es>
2026-08-27 17:36:01 -0300

    Generate a CreateMessage command
    co-authored-by: Miles Georgi <azimux@gmail.com>

--- spec/foobara/discord_api_spec.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe Foobara::DiscordApi do
-  it "has a version number" do
-    expect(Foobara::DiscordApi::VERSION).to_not be_nil
-  end
-end
--- src/foobara/discord_api.rb
+++
@@ -1,4 +0,0 @@
-module Foobara
-  module DiscordApi
-  end
-end
