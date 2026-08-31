aacaadf137788bb1e4988ac71b834aeb8637c547
Nicolas <nicolasalejocampos@outlook.es>
2026-08-31 19:29:18 -0300

    Actually comunicate with Discord successfully
    co-authored-by: Miles Georgi <azimux@gmail.com>

--- spec/create_message_spec.rb
+++ spec/create_message_spec.rb
@@ -6,15 +6,14 @@ RSpec.describe Foobara::DiscordApi::CreateMessage do
   let(:errors_hash) { outcome.errors_hash }
 
   let(:inputs) do
-    { channel_id: , content: }
+    { channel_id:, content: }
   end
 
-  let(:channel_id) { "1234567890" }
+  let(:channel_id) { ENV["DISCORD_CHANNEL_ID"] }
   let(:content) { "Hello, World!" }
 
-  
 
-  it "is successful", vcr:{ record: :once} do
+  it "is successful", vcr: { record: :once } do
     expect(outcome).to be_success
     expect(result).to eq("bar")
   end
