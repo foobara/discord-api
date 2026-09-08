0c7091df09bcf087c4a5c9abafc59a3ad6b786d4
Nicolas <nicolasalejocampos@outlook.es>
2026-09-08 15:41:05 -0300

    Complete the User model

--- src/types/user.rb
+++ src/types/user.rb
@@ -1,7 +1,10 @@
+require_relative "user_primary_guild"
+require_relative "collectible"
+require_relative "flag"
+
 module Foobara
   module DiscordApi
     class User < Foobara::Model
-      # "author":{"banner_color":null,"clan":null,"primary_guild":null}
       attributes do
         id :string, :required
         username :string, :required
@@ -11,16 +14,16 @@ module Foobara
         banner_color :string, :allow_nil
         clan :string, :allow_nil
         primary_guild [UserPrimaryGuild], :allow_nil
-        public_flags :integer, :allow_nil
-        flags :integer, :allow_nil
+        public_flags :integer, :allow_nil, one_of: FLAG
+        flags :integer, :allow_nil, one_of: FLAG
         bot :boolean, :allow_nil
         banner :string, :allow_nil
         accent_color :integer, :allow_nil
         global_name :string, :allow_nil
         collectibles [Collectible], :allow_nil
-        display_name_styles :string, :allow_nil
+        display_name_styles :duck, :allow_nil # TODO: check if the proper type is in the documentation yet and replace duck with it if so.
         banner_color :string, :allow_nil
-        clan :string, :allow_nil
+        clan :duck, :allow_ni # TODO: check if the proper type is in the documentation yet and replace duck with it if so.
         primary_guild [UserPrimaryGuild], :allow_nil
       end
     end
