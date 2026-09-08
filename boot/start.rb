d6b4b366d24e19c185b46eafb82af2cbe7a963d6
Nicolas <nicolasalejocampos@outlook.es>
2026-09-08 16:48:35 -0300

    Enable branch coverage
    co-authored-by: Miles Georgi <azimux@gmail.com>

--- boot/start.rb
+++ boot/start.rb
@@ -2,7 +2,9 @@ ENV["FOOBARA_ENV"] ||= "development"
 
 require "bundler/setup"
 
+# simplecov:disable
 if ["development", "test"].include?(ENV["FOOBARA_ENV"])
+  # simplecov:enable
   require "pry"
   require "pry-byebug"
 end
